09c18d7bf05c2d07bed38902779d834c9795e126
Uyen Truong <haiuyentruong@gmail.com>
2020-07-14 23:12:15 +0300

    1.2

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -5,7 +5,7 @@ const Header = (props) => {
   return <div><h1>{props.course}</h1></div>
 }
 
-const Content = (props) => {
+const Part = (props) => {
   return <div><p>{props.part} {props.exercise}</p></div>
 }
 
@@ -21,19 +21,26 @@ const App = () => {
   const exercises2 = 7
   const part3 = 'State of a component'
   const exercises3 = 14
-
+  
+  const Content = () => {
+    return (
+      <div>
+        <Part 
+          part={part1}
+          exercise={exercises1} />
+        <Part 
+          part={part2}
+          exercise={exercises2} />
+        <Part 
+          part={part3}
+          exercise={exercises3} />
+      </div>
+    )
+  }
   return (
     <div>
       <Header course={course} />
-      <Content 
-        part={part1}
-        exercise={exercises1} />
-      <Content 
-        part={part2}
-        exercise={exercises2} />
-      <Content 
-        part={part3}
-        exercise={exercises3} />
+      <Content />
       <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   )
