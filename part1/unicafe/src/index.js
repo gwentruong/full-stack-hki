0ac7bf16733a99b424369cb1f2e47ed542d228e7
Uyen Truong <haiuyentruong@gmail.com>
2020-08-05 16:05:20 +0300

    Ex 1.7

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -6,20 +6,47 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  
+
+  const getCount = () => {
+    return good + neutral + bad
+  }
+  const getAll = () => {
+    return good*1 + neutral*0 + bad*(-1)
+  }
+
+  const getAverage = () => {
+    return getAll()/getCount()
+  }
+
+  const getPositive = () => {
+    return (good*100)/getCount()
+  }
+
   return (
     <div>
       <h1>give feedback</h1>
       <div>
-        <button>good</button>
-        <button>neutral</button>
-        <button>bad</button>
+        <button
+          onClick={() => setGood(good+1)}>
+            good
+        </button>
+        <button
+          onClick={() => setNeutral(neutral+1)}>
+            neutral
+        </button>
+        <button
+          onClick={() => setBad(bad+1)}>
+            bad
+        </button>
       </div>
       <h1>statistics</h1>
       <div>
         <p>good {good}</p>
         <p>neutral {neutral}</p>
         <p>bad {bad}</p>
+        <p>all {getCount()}</p>
+        <p>average {getAverage()}</p>
+        <p>positive {getPositive()}</p>
       </div>
     </div>
   )
