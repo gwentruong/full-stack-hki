69285d14781b3c1d68743358ab1c03e6aafd1549
Uyen Truong <haiuyentruong@gmail.com>
2020-08-09 23:51:38 +0300

    2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,10 +1,18 @@
-import React, { useState } from 'react'
-import Person from './components/Person'
+import React, { useState, useRef } from 'react'
+import Contacts from './components/Contacts'
+import Filter from './components/Filter'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([]) 
+  const [ persons, setPersons ] = useState([
+    { id: 1, name: 'Arto Hellas', number: '040-123456' },
+    { id: 2, name: 'Ada Lovelace', number: '39-44-5323523' },
+    { id: 3, name: 'Dan Abramov', number: '12-43-234345' },
+    { id: 4, name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ]) 
   const [ newName, setNewName ] = useState('')
-  const [ newNumber, setNewNumber] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+  const [ filterText, setFilterText ] = useState('')
+  const personsRef = useRef(persons)
 
   const addContact = (event) => {
     event.preventDefault()
@@ -24,6 +32,10 @@ const App = () => {
     setNewNumber('')
   }
 
+  const dynamicSearch = (text) => {
+    personsRef.current = text ? persons.filter(p => p.name.includes(text)) : persons
+  }
+
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
@@ -31,9 +43,20 @@ const App = () => {
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
   }
+
+  const handleFilterChange = (event) => {
+    setFilterText(event.target.value)
+    dynamicSearch(event.target.value)
+  }
+
+
   return (
     <div>
       <h2>Phonebook</h2>
+        <Filter 
+          value={filterText}
+          onChange={handleFilterChange} />
+      <h2>add a new</h2>
       <form>
         <div>
           name: <input 
@@ -50,9 +73,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <div>
-        {persons.map(person => <Person key={person.id} person={person} />)}
-      </div>
+      <Contacts contacts={personsRef.current} />
     </div>
   )
 }
