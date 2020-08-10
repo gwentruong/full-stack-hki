3b6b48d408f036c4367a36edc9fcd87eeff68ec2
Uyen Truong <haiuyentruong@gmail.com>
2020-08-10 18:12:26 +0300

    Print each country info 2.12

--- part2/countries/src/components/Countries.js
+++ part2/countries/src/components/Countries.js
@@ -3,16 +3,31 @@ import React from 'react'
 const Country = ({country}) => {
     return (
         <div>
-            {country.name}
+            <h2>{country.name}</h2>
+            <div>
+                capital {country.capital}<br/>
+                population {country.population}
+            </div>
+            <h3>Languages</h3>
+            <ul>
+                {country.languages.map((language, index) => <li key={index}>{language.name}</li>)}
+            </ul>
+            <div>
+                <img src={country.flag} alt={country.name} width="120" height="100"/>
+            </div>
         </div>
     )
 }
+
 const Countries = ({countries}) => {
     return (
         <div>
-            { countries.length
-                ? countries.map((country, index) => <Country key={index} country={country} />)
-                : "Too many results, please specify another filter"}
+            { !countries.length
+                ? "Too many results, please specify another filter"
+                : (countries.length !== 1
+                    ? countries.map((country, index) => <li key={index}>{country.name}</li>)
+                    : countries.map((country, index) => <Country key={index} country={country} />))
+            }
         </div>
     )
 }
